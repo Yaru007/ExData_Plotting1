698f2df14be83a78022d0f3da34c75de9709e029
Yaru007 <yshi31@uic.edu>
2015-07-07 16:58:46 -0500

    update the code for p1

--- plot4.R
+++ plot4.R
@@ -1,3 +1,22 @@
+#import the data
+mydata = read.table("C:/Users/yshi31/Documents/coursera_course/household_power_consumption.txt",header = TRUE, sep = ";")
+#update the date format
+mydata$Date = as.character(mydata$Date)
+mydata$Date = as.Date(mydata$Date,format = "%d/%m/%Y")
+#view(mydata)
+#truncate the data
+mydata = subset(mydata,Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
+#edit the dat
+mydata$Global_active_power = as.numeric(as.character(mydata$Global_active_power))
+mydata$Global_reactive_power = as.numeric(as.character(mydata$Global_reactive_power))
+mydata$Voltage = as.numeric(as.character(mydata$Voltage))
+mydata$Sub_metering_1 = as.numeric(as.character(mydata$Sub_metering_1))
+mydata$Sub_metering_2 = as.numeric(as.character(mydata$Sub_metering_2))
+mydata$Sub_metering_3 = as.numeric(as.character(mydata$Sub_metering_3))
+
+mydata$DateTime = paste(mydata$Date,mydata$Time,sep=",")
+mydata$DateTime = strptime(mydata$DateTime,format="%Y-%m-%d,%H:%M:%S")
+
 #plot4
 png("C:/Users/yshi31/Documents/coursera_course/ExData_Plotting1/plot4.png",width = 480, height = 480)
 par(mfrow = c(2, 2))
